f938f1b8fc42e1ac4384f36b07f9bb0de68ad170
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-05-08 22:12:28 +0700

    Edit job detail

--- resources/views/clients/accounts/job/my-jobs.blade.php
+++ resources/views/clients/accounts/job/my-jobs.blade.php
@@ -28,7 +28,7 @@
                         </svg>
                         <div class="alert alert-success alert-dismissible d-flex align-items-center " role="alert">
                             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img"
-                                 aria-label="Success:">
+                                aria-label="Success:">
                                 <use xlink:href="#check-circle-fill" />
                             </svg>
                             <div>
@@ -47,53 +47,62 @@
                                     <h3 class="fs-4 mb-1">My Jobs</h3>
                                 </div>
                                 <div style="margin-top: -10px;">
-                                    <a href="{{route('account.createJob')}}" class="btn btn-primary">Post a Job</a>
+                                    <a href="{{ route('account.createJob') }}" class="btn btn-primary">Post a Job</a>
                                 </div>
 
                             </div>
                             <div class="table-responsive">
                                 <table class="table ">
                                     <thead class="bg-light">
-                                    <tr>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Job Created</th>
-                                        <th scope="col">Applicants</th>
-                                        <th scope="col">Status</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
+                                        <tr>
+                                            <th scope="col">Title</th>
+                                            <th scope="col">Job Created</th>
+                                            <th scope="col">Applicants</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
                                     </thead>
                                     <tbody class="border-0">
-                                    @if($jobs->isNotEmpty())
-                                        @foreach($jobs as $job)
-                                            <tr class="active">
-                                                <td>
-                                                    <div class="job-name fw-500">{{$job->title}}</div>
-                                                    <div class="info1">{{$job->jobType->name}} . {{$job->location}}</div>
-                                                </td>
-                                                <td>{{ \Carbon\Carbon::parse($job->create_at)->format('d M, Y') }}</td>
-                                                <td>130 Applications</td>
-                                                <td>
-                                                    @if($job->status ==1 )
-                                                        <div class="job-status text-capitalize">Active</div>
-                                                    @else
-                                                        <div class="job-status text-capitalize">Block</div>
-                                                    @endif
-                                                </td>
-                                                <td>
-                                                    <div class="action-dots float-end">
-                                                        <a href="#" class="" data-bs-toggle="dropdown" aria-expanded="false">
-                                                            <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-                                                        </a>
-                                                        <ul class="dropdown-menu dropdown-menu-end">
-                                                            <li><a class="dropdown-item" href="job-detail.html"> <i class="fa fa-eye" aria-hidden="true"></i> View</a></li>
-                                                            <li><a class="dropdown-item" href="#"><i class="fa fa-edit" aria-hidden="true"></i> Edit</a></li>
-                                                            <li><a class="dropdown-item" href="#"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
-                                                        </ul>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    @endif
+                                        @if ($jobs->isNotEmpty())
+                                            @foreach ($jobs as $job)
+                                                <tr class="active">
+                                                    <td>
+                                                        <div class="job-name fw-500">{{ $job->title }}</div>
+                                                        <div class="info1">{{ $job->jobType->name }} . {{ $job->location }}
+                                                        </div>
+                                                    </td>
+                                                    <td>{{ \Carbon\Carbon::parse($job->create_at)->format('d M, Y') }}</td>
+                                                    <td>130 Applications</td>
+                                                    <td>
+                                                        @if ($job->status == 1)
+                                                            <div class="job-status text-capitalize">Active</div>
+                                                        @else
+                                                            <div class="job-status text-capitalize">Block</div>
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                        <div class="action-dots float-end">
+                                                            <button class="btn" data-bs-toggle="dropdown"
+                                                                aria-expanded="false">
+                                                                <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                                                            </button>
+                                                            <ul class="dropdown-menu dropdown-menu-end">
+                                                                <li><a class="dropdown-item" href="job-detail.html"> <i
+                                                                            class="fa fa-eye" aria-hidden="true"></i>
+                                                                        View</a></li>
+                                                                <li><a class="dropdown-item"
+                                                                        href="{{ route('account.editJob', $job->id) }}"><i
+                                                                            class="fa fa-edit" aria-hidden="true"></i>
+                                                                        Edit</a></li>
+                                                                <li><a class="dropdown-item" href="#"><i
+                                                                            class="fa fa-trash" aria-hidden="true"></i>
+                                                                        Remove</a></li>
+                                                            </ul>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        @endif
                                     </tbody>
                                 </table>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::group(['prefix'=> 'account'], function (){
         Route::get('/create-job', [AccountController::class, 'createJob'])->name('account.createJob');
         Route::post('/save-job', [AccountController::class, 'saveJob'])->name('account.saveJob');
         Route::get('/my-jobs', [AccountController::class, 'listJobs'])->name('account.myJob');
+        Route::get('/my-jobs/edit/{jobId}', [AccountController::class, 'editJob'])->name('account.editJob');
+        Route::post('/update-job/{jobId}', [AccountController::class, 'updateJob'])->name('account.updateJob');
 
     });
 });
