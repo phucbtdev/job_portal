811e2b9b4664bb5b05f41430700cdda3e4e447a3
Phúc Dev <phucbuitrong.dev@gmail.com>
2024-05-13 07:20:10 +0700

    Search job in homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,11 +11,13 @@ class HomeController extends Controller
     public function index ()
     {
         $categories = Category::where('status', 1)->orderBy('name', 'ASC')->take(8)->get();
+        $categoriesNew = Category::where('status', 1)->orderBy('name', 'ASC')->get();
         $featuredJobs = Job::where('status' ,1)->orderBy('created_at','DESC')->with('jobType')->where('isFeatured' ,1)->take(6)->get();
         $latestJobs = Job::where('status' ,1)->orderBy('created_at','DESC')->with('jobType')->take(6)->get();
 
         return view('clients.home',[
             'categories' => $categories,
+            'categoriesNew' => $categoriesNew,
             'featuredJobs'=> $featuredJobs,
             'latestJobs' =>$latestJobs
         ]);
